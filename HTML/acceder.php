1a4b1dfc0c97817f209a1a6a96e5abd7290cf70b
Virillo <albertomedvel@gmail.com>
2022-12-04 18:39:34 +0100

    comentarios acceder
    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- HTML/acceder.php
+++ HTML/acceder.php
@@ -11,29 +11,46 @@
 
 <body>
    <?php
+//    Se importa el header
        require 'header.php';
        
     ?>  
       <section class="forms-section ">
-        <!-- login -->
+
+        <!-- Formularios para inicio de sesión y registro -->
+
         <div class="forms">
+
+        <!-- Div del formulario para inicio de sesión,esta active para que aparezca por predeterminado -->
+
             <div class="form-wrapper is-active ">
                 <button type="button" class="switcher switcher-login">
                     Iniciar Sesión
                     <span class="underline"></span>
                 </button>
+
+                <!-- Formulario que envia la informacion a login.php -->
+
                 <form class="form form-login" id="form-login" method="POST" action="../Server/login.php">
                     <fieldset>
+
+                        <!-- Campo usuario -->
 
                         <div class="input-block">
                             <label for="usuario_login">Usuario</label>
                             <input type="text" name="usuario_login" id="usuario_login"><br>
                         </div>
+
+                        <!-- Campo contraseña -->
+
                         <div class="input-block">
                             <label for="contrasena_login">Contraseña</label>
                             <input name="contrasena_login" id="contrasena_login" type="password" required>
                         </div>
                     </fieldset>
+
+                    <!-- Butones para enviar datos y resetear formulario -->
+
                     <button type="submit" name="iniciar_sesion" id="enviar_login" class="btn-login">Iniciar Sesion</button>
                     <button type="reset" class="btn-login">Limpiar</button>
 
@@ -41,7 +58,7 @@
             </div>
 
 
-            <!-- Registor -->
+            <!-- Div del formulario pararegistro, por defecto esta en segundo plano -->
 
             <div class="form-wrapper  ">
                 <button type="button" class="switcher switcher-signup">
@@ -50,36 +67,52 @@
                 </button>
                 <form class="form form-signup" method="POST" action="../Server/register.php">
                     <fieldset>
+
+                        <!-- Campo usuario -->
+
                         <div class="input-block">
                             <label for="usuario_signup">Usuario</label>
                             <input type="text" name="usuario_signup" id="usuario_signup" required>
                         </div>
+
+                         <!-- Campo nombre real -->
 
                         <div class="input-block">
                             <label for="nombre_real">Nombre</label>
                             <input type="text" name="nombre_real" id="nombre_real">
                         </div>
 
+                         <!-- Campo apellidos -->
+
                         <div class="input-block">
                             <label for="apellidos_signup">Apellidos</label>
                             <input type="text" name="apellidos_signup" id="apellidos_signup">
                         </div>
 
+                         <!-- Campo email -->
 
                         <div class="input-block">
                             <label for="email_signup">E-mail</label>
                             <input type="email" name="email_signup" id="email_signup" required>
                         </div>
+
+                         <!-- Campo contraseña -->
+
                         <div class="input-block">
                             <label for="contrasena_signup">Contraseña</label>
                             <input type="password" name="contrasena_signup" id="contrasena_signup" required>
                         </div>
+
+                         <!-- Campo confirmar contraseña -->
+
                         <div class="input-block">
                             <label for="confirmar_contrasena_signup">Confirmar Contraseña</label>
                             <input type="password" name="confirmar_contrasena_signup" id="confirmar_contrasena_signup" required>
                         </div>
                     </fieldset>
                     
+                    <!-- Butones para enviar datos y resetear formulario -->
+
                     <button type="submit" name="registrarse" id="enviar_register" class="btn-signup">Registrarse</button>
                     <button type="reset" class="btn-signup">Limpiar</button>
                 </form>
@@ -88,9 +121,10 @@
     </section>
 </body>
 
+        <!-- Script para dar primer plano al div que deseemos dependiendo de si hacemos click en "INICIAR SESIÓN" O "REGISTRARSE" -->
+
         <script>
             const switchers = [...document.querySelectorAll('.switcher')]
-    
             switchers.forEach(item => {
                 item.addEventListener('click', function () {
                     switchers.forEach(item => item.parentElement.classList.remove('is-active'))
