908c8d4a2bfc52b1e301d667ab80e2c1d40ae65d
Samir <rhazisamir16@gmail.com>
2022-12-11 03:42:21 +0100

    borrar cuenta

--- cessar/Server/borrar_cuenta.php
+++ cessar/Server/borrar_cuenta.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
+session_start();
 include 'conectarse.php';
+$usuario = $_SESSION["username"]['usuario'];
+$MyBBDD->consulta("UPDATE posts SET post_autor='admin' where post_autor='$usuario'");
 
 // Query para borrar la fila de el usuario en la BBDD
 
-$MyBBDD->consulta("DELETE FROM registro ");
+$MyBBDD->consulta("DELETE FROM registro where usuario='$usuario'");
+
 
 // Redirección a pagina principal
 
 header("location: ../HTML/index.php");
-?>
- 
